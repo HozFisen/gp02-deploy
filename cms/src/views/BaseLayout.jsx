75da3ec3b3103aee938ffb124f01e4d0a68a0b63
Haris F. <hozfisen@gmail.com>
2025-11-10 08:44:50 +0700

    feat: authorization check
    for checking whether user has token or not reroutes to login page.

--- cms/src/views/BaseLayout.jsx
+++ cms/src/views/BaseLayout.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import Login from "./Login";
 
 export default function BaseLayout() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
     return (
         <>
             <div className="p-5">
